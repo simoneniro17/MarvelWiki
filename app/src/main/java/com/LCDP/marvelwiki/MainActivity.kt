80bed64456e49575aa6ca03eb9a0ff51dd359ef4
ggwplank <brunori.lorenzo2001@gmail.com>
2023-07-11 10:16:31 +0200

    RISOLTI PROBLEMI NELLA STAMPA NELLA MAIN ACTIVITY. MANCANZA DI COMMENTI.

--- app/src/main/java/com/LCDP/marvelwiki/MainActivity.kt
+++ app/src/main/java/com/LCDP/marvelwiki/MainActivity.kt
@@ -3,7 +3,13 @@ package com.LCDP.marvelwiki
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.lifecycle.ViewModelProvider
+import com.LCDP.marvelwiki.data.model.CharacterResponse
 import com.LCDP.marvelwiki.data.repository.CharactersRepository
 import com.LCDP.marvelwiki.ui.screen.CharactersScreen
 import com.LCDP.marvelwiki.ui.screen.Navigation
@@ -22,42 +28,61 @@ class MainActivity : ComponentActivity() {
             //Per farlo ritornare come prima decommenta la riga sopra e commenta il resto sotto
 
 
-
             lateinit var charactersViewModel: CharactersViewModel
             val characterRepository = CharactersRepository()
             val characterViewModelProviderFactory = CharactersViewModelFactory(characterRepository)
-            charactersViewModel = ViewModelProvider(this, characterViewModelProviderFactory).get(CharactersViewModel::class.java)
+            charactersViewModel = ViewModelProvider(
+                this,
+                characterViewModelProviderFactory
+            ).get(CharactersViewModel::class.java)
 
 
             // Richiama il composable CharactersScreen
             CharactersScreen(charactersViewModel)
 
-            println("Ciao")
-
-
-
-                // Stampa i campi dei personaggi
-
-            when (val characters = charactersViewModel.characters.value) {
-                    is Resource.Success -> {
-                        characters.data?.characterData?.results?.forEach {  
-
-                        character ->
-                            println("Nome: ${character.name}                1111111111111111111111111111111111111111111")
-                            //println("Descrizione: ${character.description}")
-                            //println("Thumbnail: ${character.thumbnail}")
-                            //println()
-                        }
-                    }
-                    is Resource.Error -> {
-                        println("Errore: ${characters.message}")
-                    }
-                    is Resource.Loading -> {
-                        println("Caricamento...")
-                    }
-
-                    else -> {}
-                }
-            }
         }
     }
+
+
+    @Composable
+    fun CharactersScreen(charactersViewModel: CharactersViewModel) {
+
+        println("ciao")
+
+        val characters: Resource<CharacterResponse> by charactersViewModel.characters.observeAsState(
+            Resource.Loading()
+        )
+
+        when (characters) {
+            is Resource.Loading -> {
+                // Mostra il caricamento dei dati
+            }
+
+            is Resource.Success -> {
+                println("ciao1")
+                // Mostra i dati dei personaggi
+                val characterResponse = characters.data
+                val characterList = characterResponse?.characterData?.results
+                println("ciao2")
+
+                characterList?.forEach { character ->
+                    character?.let {
+                        println(character.name)
+
+                        //println("ciao3")
+                    }
+                }
+            }
+            is Resource.Error -> {
+                // Mostra un messaggio di errore
+                val errorMessage = characters.message
+                // ...
+            }
+        }
+
+        // ...
+    }
+}
+
+
+
