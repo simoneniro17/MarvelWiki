083d85a3cfb191f9684cfcb371cfc291e5507f76
ggwplank <brunori.lorenzo2001@gmail.com>
2023-07-12 10:18:08 +0200

    Aggiunti i commenti ed aggiustate le classi

--- app/src/main/java/com/LCDP/marvelwiki/data/repository/ComicsRepository.kt
+++ app/src/main/java/com/LCDP/marvelwiki/data/repository/ComicsRepository.kt
@@ -3,6 +3,6 @@ package com.LCDP.marvelwiki.data.repository
 import com.LCDP.marvelwiki.data.API.ComicInstance
 
 class ComicsRepository {
-
+    //Questo metodo chiama dalla api il metodo getcomics
     suspend fun getComics(id: Int,offset: Int) = ComicInstance.comic_api.getComics(id,offset = offset)
 }
