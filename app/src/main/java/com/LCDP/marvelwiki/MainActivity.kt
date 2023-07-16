ea6a7cb3d5cebf298372a8adfec041c44c150786
FontCD <fonteltroll06@gmail.com>
2023-07-16 16:11:07 +0200

    Tentativi col MediaPlayer

--- app/src/main/java/com/LCDP/marvelwiki/MainActivity.kt
+++ app/src/main/java/com/LCDP/marvelwiki/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.LCDP.marvelwiki
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,6 +8,9 @@ import androidx.activity.compose.setContent
 import com.LCDP.marvelwiki.ui.screen.Navigation
 
 class MainActivity : ComponentActivity() {
+
+    private var mediaPlayer : MediaPlayer?= null
+
     //APP LAUNCH
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
